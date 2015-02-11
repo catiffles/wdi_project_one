bd360ba826c16cd0bdd64bb0482d92392444112f
Susan <susan@marpy.net>
2015-02-11 17:16:44 -0500

    redirects to index page after successful log in and sign up

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,5 @@ class List < ActiveRecord::Base
   belongs_to :section
   validates :name, presence: true
   has_many :phrases, dependent: :destroy
+  belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "registrations" }
   shallow do
     resources :topics do
       resources :sections do
