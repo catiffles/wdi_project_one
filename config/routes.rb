a2ce7a87fa2892c28d203c559637d93952a85054
Susan <susan@marpy.net>
2015-02-11 15:05:53 -0500

    added a welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     end
   end
 
-  root to: 'topics#index'
+  root to: 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
